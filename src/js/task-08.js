fc753e101453420bfa7ade5b48648c834bfbf4cf
Marini23 <marina.smile1m@gmail.com>
2023-07-03 21:02:27 +0200

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(`.login-form`);
 
+
+form.addEventListener(`submit`, onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (!email.value || !password.value) {
+    alert('Всі поля повинні бути заповнені!');
+    }
+    else {
+        const data = {
+            email: email.value,
+            password: password.value,
+        };
+        console.log(data);
+        form.reset();
+    
+    }
+
+    
+}
